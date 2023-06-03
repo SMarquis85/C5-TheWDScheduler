cb7c46442c0101171bc30d311577c94f06e04874
SMarquis85 <sophienathalie_85@outlook.com>
2023-06-02 21:34:33 -0400

    updated html, css and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,15 @@
-$(function () {
+$(document).ready(function () {
   // Add a listener for click events on the save button
   $(".saveBtn").on("click", function () {
-    // Get the hour from the parent time-block's id
     var hour = $(this).parent().attr("id");
-
-    // Get the description from the textarea
     var description = $(this).siblings(".description").val();
-
-    // Save the description in local storage using the hour as the key
     localStorage.setItem(hour, description);
   });
 
   // Apply the past, present, or future class to each time block
   $(".time-block").each(function () {
-    // Get the hour from the time-block's id
     var hour = parseInt($(this).attr("id"));
-
-    // Get the current hour using Day.js
     var currentHour = parseInt(dayjs().format("H"));
-
-    // Compare the hour to the current hour and apply the appropriate class
     if (hour < currentHour) {
       $(this).addClass("past");
     } else if (hour === currentHour) {
@@ -31,32 +21,31 @@ $(function () {
 
   // Get user input from local storage and set the textarea values
   $(".time-block").each(function () {
-    // Get the hour from the time-block's id
     var hour = $(this).attr("id");
-
-    // Get the description from local storage using the hour as the key
     var description = localStorage.getItem(hour);
-
-    // Set the textarea value to the description
     $(this).find(".description").val(description);
   });
 
-  // Display the current date in the header of the page
-  var currentDate = dayjs().format("DDDD, MMMM, YYYY");
-  $("#currentDate").text(currentDate);
-});
+   // Display the current day and date
+   $("#currentDay").text(dayjs().format("dddd"));
+   $("#currentDate").text(dayjs().format("MMMM D, YYYY"));
+ });
+ 
+// Added to properly display current day
+$(document).ready(function() {
+  function updateCurrentDay() {
+    var currentDay = dayjs().format("dddd");
+    $("#currentDay").text(currentDay);
+  }
 
+  function updateCurrentDate() {
+    var currentDate = dayjs().format("MMMM D, YYYY");
+    $("#currentDate").text(currentDate);
+  }
 
-$(document).ready(function() { 
-  var currentDateElement = $('#currentDate'); 
+  updateCurrentDay();
+  updateCurrentDate();
 
-function updateCurrentDate() { 
-  var currentDate = dayjs().format("dddd, MMMM, YYYY"); 
-  currentDateElement.text(currentDate); 
-}
-
-updateCurrentDate(); 
-
-setInterval(updateCurrentDate, 1000); 
-
+  setInterval(updateCurrentDay, 1000);
+  setInterval(updateCurrentDate, 1000);
 });
