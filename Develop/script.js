a0e32335e7e91920ee80160d1b6a371085425a4a
SMarquis85 <sophienathalie_85@outlook.com>
2023-06-04 16:26:19 -0400

    updated html, css and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Function to update the time block classes
+  function updateColorCoding() {
+    var currentTime = dayjs();
+
+    $(".time-block").each(function () {
+      var hour = parseInt($(this).attr("id").split("-")[1]);
+      var timeBlockStart = dayjs().set("hour", hour).startOf("hour");
+      var timeBlockEnd = dayjs().set("hour", hour).endOf("hour");
+
+      $(this).removeClass("past present future");
+
+      if (currentTime.isBefore(timeBlockStart)) {
+        $(this).addClass("future");
+      } else if (currentTime.isBetween(timeBlockStart, timeBlockEnd)) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("past");
+      }
+    });
+  }
+
   // Add a listener for click events on the save button
   $(".saveBtn").on("click", function () {
     var hour = $(this).parent().attr("id");
@@ -15,13 +36,20 @@ $(document).ready(function() {
 
   // Display the current day and date
   function updateCurrentDate() {
-    var currentDay = dayjs().format("dddd");
-    var currentDate = dayjs().format("MMMM D, YYYY");
-    var currentDateTime = currentDay + ' ' + currentDate;
-    $("#currentDate").text(currentDateTime);
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    $('#currentDate').text(currentDate);
   }
+  
+  $(document).ready(function() {
+    updateCurrentDate();
+  });
 
+  // Update the time block classes every minute
+  setInterval(updateColorCoding, 60000);
+
+  // Initial update of time block classes
+  updateColorCoding();
+
+  // Update the current date in the header
   updateCurrentDate();
-
-  setInterval(updateCurrentDate, 1000);
 });
