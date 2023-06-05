8d443e9ab7fa3135bb6b92a7b06a4d05083cc06f
SMarquis85 <sophienathalie_85@outlook.com>
2023-06-05 13:35:53 -0400

    updated js, images and README.md file

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Function to update the time block classes
   function updateColorCoding() {
-    var currentHour = dayjs().format('H');
-
+    var currentHour = dayjs().hour();
+  
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
+  
       if (blockHour < currentHour) {
         $(this).removeClass("present future").addClass("past");
       } else if (blockHour === currentHour) {
@@ -30,16 +30,18 @@ $(document).ready(function () {
     $(this).find(".description").val(description);
   });
 
+  // Call the initial update of time block classes
+  updateColorCoding();
+  // Update the current date
+  updateCurrentDate();
+
   // Display the current day and date
   function updateCurrentDate() {
     var currentDate = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDate').text(currentDate);
   }
-
-  // Call the initial update of time block classes
-  updateColorCoding();
-  updateCurrentDate();
-
   // Update the time block classes every minute
   setInterval(updateColorCoding, 60000);
 });
+
+
